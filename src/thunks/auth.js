7006a885110a1de9416e65a3fcb1b112b9cf4490
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-12 16:17:29 +0800

    Set up thunks and actions files

---
+++ src/thunks/auth.js
@@ -0,0 +1,21 @@
+export const signup = async (dispatch, signupParams) => {
+  const path = '';
+  try {
+    // set current user data
+  } catch (err) {
+    // set error data
+  }
+};
+
+export const signin = async (dispatch, signinParams) => {
+  const path = '';
+  try {
+    // set current user data in context
+  } catch (err) {
+    // set error in context
+  }
+};
+
+export const signout = async (dispatch) => {
+  // set user data in context
+};
