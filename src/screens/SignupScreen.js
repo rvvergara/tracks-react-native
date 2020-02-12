80ac37ed9e29e155b9f7bff0335a9db3d4f0c73f
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-12 15:00:17 +0800

    Center SignupScreen box vertically

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import {
   StyleSheet,
+  View,
 } from 'react-native';
 import { Text, Input, Button } from 'react-native-elements';
 import Spacer from '../components/Spacer';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 250,
+  },
+});
 
 const SignupScreen = ({ navigation }) => (
-  <>
+  <View style={styles.container}>
     <Spacer>
       <Text h3>Sign Up For Tracker</Text>
     </Spacer>
@@ -18,7 +25,11 @@ const SignupScreen = ({ navigation }) => (
     <Spacer>
       <Button title="Sign up" />
     </Spacer>
-  </>
+  </View>
 );
+
+SignupScreen.navigationOptions = () => ({
+  headerShown: false,
+});
 
 export default SignupScreen;
