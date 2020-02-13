46667cb1d5242eec2ba7012b479767039e1bffd9
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-13 09:15:41 +0800

    Link react-native/async-storage

--- src/thunks/auth.js
+++ src/thunks/auth.js
@@ -1,13 +1,15 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import trackerAPI from '../utils/tracker';
-import { setAuthToken } from '../actions/auth';
-import { setError } from '../actions/error';
+import {setAuthToken} from '../actions/auth';
+import {setError} from '../actions/error';
 
 export const signup = async (signupDispatch, errorDispatch, signupParams) => {
   const path = '/signup';
   try {
     const res = await trackerAPI.post(path, signupParams);
-    const { token } = res.data;
-    signupDispatch(setAuthToken(token));
+    const {token} = await res.data;
+    await signupDispatch(setAuthToken(token));
+    await AsyncStorage.setItem('token', token);
     return token;
   } catch (err) {
     if (err.response.data.includes('email')) {
