dec34d5f1662427f033ba5f66a50299cb3b6a1c5
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2020-02-13 13:01:28 +0800

    Extract UserForm logic

---
+++ src/components/UserForm.js
@@ -0,0 +1,102 @@
+import React, {useState, useContext, useEffect} from 'react';
+import PropTypes from 'prop-types';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import {Text, Input, Button} from 'react-native-elements';
+import {withNavigation} from 'react-navigation';
+import Spacer from './Spacer';
+import {Context as AuthContext} from '../context/AuthContext';
+import {Context as ErrorContext} from '../context/ErrorContext';
+import {signup, signin} from '../thunks/auth';
+import {setError} from '../actions/error';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    marginBottom: 150,
+  },
+  error: {
+    margin: 15,
+    color: 'red',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  link: {
+    color: 'blue',
+  },
+});
+
+const UserForm = ({navigation}) => {
+  const {routeName} = navigation.state;
+  const linkText = routeName === 'Signup' ? 'Already have an account? Sign in instead' : "Don't have an account yet? Sign up instead";
+  const titleText = routeName === 'Signup' ? 'Sign Up For Tracker' : 'Sign In To Your Account';
+  const buttonText = routeName === 'Signup' ? 'Signup' : 'Signin';
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const {dispatch: authDispatch} = useContext(AuthContext);
+
+  const {state: errorState, dispatch: errorDispatch} = useContext(ErrorContext);
+
+  useEffect(
+    () => () => {
+      errorDispatch(setError(''));
+    },
+    [],
+  );
+
+  useEffect(() => {
+    navigation.addListener('didFocus', () => {
+      errorDispatch(setError(''));
+    });
+  }, []);
+
+  const handleSubmit = async () => {
+    const auth = routeName === 'Signup' ? signup : signin;
+    const res = await auth(authDispatch, errorDispatch, {email, password});
+    if (res) {
+      navigation.navigate('mainFlow');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Spacer>
+        <Text h3>{titleText}</Text>
+      </Spacer>
+      <Input
+        label="Email"
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <Spacer />
+      <Input
+        label="Password"
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+      />
+      {errorState ? <Text style={styles.error}>{errorState}</Text> : null}
+      <Spacer>
+        <Button title={buttonText} onPress={handleSubmit} />
+      </Spacer>
+      <Spacer>
+        <TouchableOpacity onPress={() => navigation.navigate(routeName === 'Signup' ? 'Signin' : 'Signup')}>
+          <Text style={styles.link}>
+            { linkText }
+          </Text>
+        </TouchableOpacity>
+      </Spacer>
+    </View>
+  );
+};
+
+UserForm.propTypes = {
+  navigation: PropTypes.instanceOf(Object).isRequired,
+};
+
+export default withNavigation(UserForm);
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,93 +1,11 @@
-import React, { useState, useContext, useEffect } from 'react';
-import {
-  StyleSheet,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import { Text, Input, Button } from 'react-native-elements';
-import Spacer from '../components/Spacer';
-import { Context as AuthContext } from '../context/AuthContext';
-import { Context as ErrorContext } from '../context/ErrorContext';
-import { signup } from '../thunks/auth';
-import {setError} from '../actions/error';
+import React from 'react';
+import UserForm from '../components/UserForm';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    marginBottom: 150,
-  },
-  error: {
-    margin: 15,
-    color: 'red',
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  link: {
-    color: 'blue',
-
-  },
-});
-
-const SignupScreen = ({ navigation }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const { dispatch: signupDispatch } = useContext(AuthContext);
-
-
-  const { state: errorState, dispatch: errorDispatch } = useContext(ErrorContext);
-
-
-  useEffect(() => () => {
-    errorDispatch(setError(''));
-  }, []);
-
-  const handleSignup = async () => {
-    const res = await signup(signupDispatch, errorDispatch, { email, password });
-    if (res) {
-      navigation.navigate('mainFlow');
-    }
-  };
-
-  return (
-    <View style={styles.container}>
-      <Spacer>
-        <Text h3>Sign Up For Tracker</Text>
-      </Spacer>
-      <Input
-        label="Email"
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize="none"
-        autoCorrect={false}
-      />
-      <Spacer />
-      <Input
-        label="Password"
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize="none"
-        autoCorrect={false}
-        secureTextEntry
-      />
-      {errorState ? <Text style={styles.error}>{errorState}</Text> : null}
-      <Spacer>
-        <Button
-          title="Sign up"
-          onPress={handleSignup}
-        />
-      </Spacer>
-      <Spacer>
-        <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
-          <Text style={styles.link}>
-            Already Have An Account? Sign in instead
-          </Text>
-        </TouchableOpacity>
-      </Spacer>
-    </View>
-  );
-};
+const SignupScreen = () => (
+  <>
+    <UserForm />
+  </>
+);
 
 SignupScreen.navigationOptions = () => ({
   headerShown: false,
